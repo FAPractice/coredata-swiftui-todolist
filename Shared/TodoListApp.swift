db4c4e533f98651ea42a7f6c3abd5c1117a41ca1
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-03-30 10:39:36 +0530

    Initial Commit

---
+++ Shared/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  Shared
+//
+//  Created by Harsh Chaturvedi on 30/03/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
